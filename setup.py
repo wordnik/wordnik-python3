a9a656af6a27625935cfe89402c8c1df42c00b86
Russell Horton <russ@wordnik.com>
2013-01-03 22:53:37 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name='wordnik-py3', version='2.1', py_modules=['wordnik'],
+      description='Wordnik API for Python3', author='Russell Horton',
+      author_email='russ@wordnik.com', url='http://developer.wordnik.com',
+      packages=find_packages())
